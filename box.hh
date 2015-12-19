83266e034bd8828d183cdb13666d57e0da177f34
Bob <bob@bob.com>
2015-12-19 11:14:21 -0600

    Hotfix for duplicate parameter

--- box.hh
+++ box.hh
@@ -5,7 +5,6 @@ class Box
       double length;   // Length of a box
       double width;    // width of a box
       double height;   // Height of a box
-      int height;
 
       void report();
       void setWidth( double inWidth) ;
