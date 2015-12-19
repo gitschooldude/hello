432a095115614be22ec566e28e7f4c0c0dc4ce68
Dan <dan@dan.com>
2015-12-19 05:04:49 -0600

    Added another height parameter.

--- box.hh
+++ box.hh
@@ -5,6 +5,7 @@ class Box
       double length;   // Length of a box
       double width;    // width of a box
       double height;   // Height of a box
+      int height;
 
       void report();
       void setWidth( double inWidth) ;
