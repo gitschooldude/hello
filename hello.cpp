8ff156bd27b9a105c4f2a73c12f3d9e90a91b668
Dan <dan@dan.com>
2016-08-28 09:14:29 -0500

    removed box1

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 int main(int argc, char** argv)
 {  
-  Box box1;
   Box box2;
   Box boxA;
   Box boxB;
@@ -12,10 +11,7 @@ int main(int argc, char** argv)
   Box boxE;
   Box boxF;
 
-  box1.setWidth(15.0);
   cout<<"Hello World!" << endl;
-  cout<<"Box1:" << endl;
-  box1.report();
   cout<<"Box2:" << endl;
   box2.report();
   cout<<"BoxA:" << endl;
